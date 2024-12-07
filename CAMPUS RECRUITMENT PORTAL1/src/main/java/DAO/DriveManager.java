e168d0a8ec5725a644c1ba111b1f85b724d64757
Sadiya-Khanam <khanamsadiya1234@gmail.com>
2024-12-07 19:36:28 +0530

    add files

---
+++ CAMPUS RECRUITMENT PORTAL1/src/main/java/DAO/DriveManager.java
@@ -0,0 +1,18 @@
+package DAO;
+import java.sql.DriveManager;
+import java.sql.Connection;
+
+public class DriveManager {
+
+	public static void main(String[] args) {
+		Connection con= DriveManager.getConnection()
+		// TODO Auto-generated method stub
+
+	}
+
+	public static Connection getConnection(String string, String string2, String string3) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
